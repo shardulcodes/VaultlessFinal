0a6682a7bdbebd69f3eafa8e2112d1346fa131e6
shardulcodes <shekatkarshardul@gmail.com>
2025-07-16 15:49:03 +0530

    database correction

--- app.py
+++ app.py
@@ -222,7 +222,6 @@ def change_password():
 
 
 # ==================== Run App ====================
-if __name__ == '__main__':
+if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', port=5000, debug=True)
